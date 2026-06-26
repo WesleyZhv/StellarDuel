3915cb6ffc5636b8ea28ab2c2957b5ce09f5ae0a
WesleyZhv <wesleyzhv@gmail.com>
2026-06-26 03:24:31 +0200

    View : MenuView stylisé thème spatial

--- src/main/java/com/stellarduel/view/MenuView.java
+++ src/main/java/com/stellarduel/view/MenuView.java
@@ -4,6 +4,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import  javafx.geometry.Pos;
 
 public class MenuView extends VBox{
 
@@ -17,6 +18,14 @@ public class MenuView extends VBox{
         this.boutonJoueur = new Button("Jouer");
         setSpacing(10);
         getChildren().addAll(this.titre, this.champNom, this.boutonJoueur);
+        setAlignment(Pos.CENTER);
+        this.champNom.setPromptText("Entrez votre nom");
+        this.titre.setStyle("-fx-font-size: 32px;");
+        setStyle("-fx-background-color: #0a0a1a;");
+        this.titre.setStyle("-fx-font-size: 48px; -fx-text-fill: #00aaff; -fx-font-weight: bold;");
+        this.champNom.setMaxWidth(300);
+        this.boutonJoueur.setStyle("-fx-background-color: #00aaff; -fx-text-fill: white; -fx-font-size: 16px; -fx-padding: 10 30;");
+        this.boutonJoueur.setMinWidth(150);
     }
 
     public TextField getChampNom(){
