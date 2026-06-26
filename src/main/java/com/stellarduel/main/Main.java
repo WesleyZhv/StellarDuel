7563051e85ccc5bdb290344049e05d4987f92c03
WesleyZhv <wesleyzhv@gmail.com>
2026-06-26 02:55:52 +0200

    Main : lancement JavaFX fonctionnel

--- src/main/java/com/stellarduel/main/Main.java
+++ src/main/java/com/stellarduel/main/Main.java
@@ -1,7 +1,25 @@
 package com.stellarduel.main;
 
-public class Main {
-    public static void main(String[] args){
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.collections.FXCollections;
+import javafx.scene.control.Alert;
 
+public class Main extends Application{
+
+    public void start(Stage stage){
+    stage.setTitle("Stellar Duel");
+
+
+
+    stage.setScene(new Scene(new javafx.scene.layout.StackPane(), 800, 600));
+    stage.show();
+
+    }
+
+    public static void main(String[] args){
+        launch(args);
     }
 }
